7b5ea749c2ea9c333738246657f6b50a9c366839
DanielKE <elder.dk@gmail.com>
2020-07-12 19:22:33 +0900

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-NSW_BAR = "https://find-a-barrister.nswbar.asn.au/"
-VIC_BAR_BASE = "https://www.vicbar.com.au/"
-VIC_BAR = "https://www.vicbar.com.au//find-barrister?term=&signed_bar_roll=All&qualifications=&keywords=&silk_or_junior=All&accredited_mediator=All&directbriefs=All&adv_accredited_mediator=All&directaccess=All&arbitration=All&gender=All&icc_accreditation=All&pss=All&items_per_page=200&sort_bef_combine=field_seniority_sort_order+ASC"
+NSW_BAR = *
+VIC_BAR_BASE = *
+VIC_BAR = *
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
 COLUMNS = ['prof_url', 'prof_pic', 'full_name', 'tel', 'ext_phone', 'mob', 'fax', 'email', 'chamber', 'date_of_admission', 'first_signed_bar_roll', 'date_appointed_silk', 'division_of_bar_roll', 'qualifications', 'previous_occupation', 'profile', 'clerk_title', 'clerk_href', 'clerk_phone', 'clerk_email', 'area_of_practice', 'arbitration_qualifications', 'other_arbitration_qualifications', 'jurisdictions']
